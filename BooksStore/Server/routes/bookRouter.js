42b404191c3e7b04b2810f1af0b2bc1961a90f62
Arunrai0752 <realarunrai0752@gmail.com>
2025-07-17 01:12:42 +0530

    Remove authentication middleware from book routes for testing purposes

--- BooksStore/Server/routes/bookRouter.js
+++ BooksStore/Server/routes/bookRouter.js
@@ -5,11 +5,11 @@ import { Protect } from "../middlewares/authmiddleware.js";
 import express from "express";
 const router = express.Router();
 
-router.get("/books",Protect, getBooks)  // Get All Books
-router.get("/:id",Protect,getBook)// Get All Books by id
-router.post("/add",Protect, addBook) // add book
-router.put("/:id",Protect, editBook) // Edit book by id
-router.delete("/:id",Protect, deleteBook) // Edit book by id
+router.get("/books", getBooks)  // Get All Books
+router.get("/:id",getBook)// Get All Books by id
+router.post("/add", addBook) // add book
+router.put("/:id", editBook) // Edit book by id
+router.delete("/:id", deleteBook) // Edit book by id
 
 
 export default router;
